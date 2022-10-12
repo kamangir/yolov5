47e8ffd667d8c49442b226d1d1a88bb8fedb192d
kamangir <arash@kamangir.net>
2022-10-11 20:26:11 -0700

    validating yolov5 - kamangir/bolt#722

--- .abcli/yolov5.sh
+++ .abcli/yolov5.sh
@@ -4,8 +4,8 @@ function yolov5() {
     local task=$(abcli_unpack_keyword $1 help)
 
     if [ $task == "help" ] ; then
-        abcli_show_usage "yolov5 task_1" \
-            "yolov5 task_1."
+        abcli_show_usage "yolov5 sync_fork" \
+            "sync yolov5 w/ upstream."
 
         if [ "$(abcli_keyword_is $2 verbose)" == true ] ; then
             python3 -m yolov5 --help
@@ -14,8 +14,12 @@ function yolov5() {
         return
     fi
 
-    if [ "$task" == "task_1" ] ; then
-        echo "wip"
+    if [ "$task" == "sync_fork" ] ; then
+        abcli_git \
+            sync_fork \
+            yolov5 \
+            master
+
         return
     fi
 
