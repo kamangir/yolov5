46410056613e233cc2dab6e76d48b9279f81ea9f
kamangir <arash@kamangir.net>
2022-10-11 20:23:03 -0700

    validating yolov5 - kamangir/bolt#722

--- .abcli/yolov5.sh
+++ .abcli/yolov5.sh
@@ -4,10 +4,8 @@ function yolov5() {
     local task=$(abcli_unpack_keyword $1 help)
 
     if [ $task == "help" ] ; then
-        abcli_help_line "yolov5 install" \
-            "install yolov5."
-        abcli_help_line "yolov5 validate" \
-            "validate yolov5."
+        abcli_help_line "yolov5 task_1" \
+            "yolov5 task_1."
 
         if [ "$(abcli_keyword_is $2 verbose)" == true ] ; then
             python3 -m yolov5 --help
@@ -16,13 +14,8 @@ function yolov5() {
         return
     fi
 
-    if [ "$task" == "install" ] ; then
-        abcli_git install yolov5
-        return
-    fi
-
-    if [ "$task" == "validate" ] ; then
-        python3 -m yolov5 validate
+    if [ "$task" == "task_1" ] ; then
+        echo "wip"
         return
     fi
 
