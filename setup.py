218120d2f2a788f9259228f564e136a142c2c709
kamangir <arash@kamangir.net>
2022-10-15 12:43:44 -0700

    kamangir.net/private: animated.gif -> list-of-images - kamangir/bolt#723

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup
 from yolov5 import *
 
 setup(
-    name=name,
-    author="kamangir",
-    version=version,
-    description=description,
-    packages=[name],
+    name=NAME,
+    author="arash@kamangir.net",
+    version=VERSION,
+    description="a bash cli for YOLOv5 🚀",
+    packages=[NAME],
 )
