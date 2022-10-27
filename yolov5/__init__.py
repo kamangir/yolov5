0caf8eb6b23b38124747fc808af31eb3b212eceb
kamangir <arash@kamangir.net>
2022-10-26 20:17:31 -0700

    validating ingest - kamangir/bolt#722

--- yolov5/__init__.py
+++ yolov5/__init__.py
@@ -1,3 +1,3 @@
 NAME = "yolov5"
 
-VERSION = "3.21.1"
+VERSION = "3.23.1"
--- yolov5/dataset/functions.py
+++ yolov5/dataset/functions.py
@@ -27,8 +27,7 @@ def adjust_dataset(filename):
     if not success:
         return success
 
-    dataset_yaml["path"] = file.path(filename)
-    dataset_yaml["val"] = "images/val"
+    dataset_yaml["path"] = "./"
 
     dataset_yaml["classes"] = dataset_yaml["names"]
     del dataset_yaml["names"]
