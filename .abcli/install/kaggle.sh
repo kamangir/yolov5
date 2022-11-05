fa4b045810a47b123aa12de38101b2bf1dd79326
kamangir <arash@kamangir.net>
2022-11-05 11:38:12 -0700

    validating yolov5 train coco128 on ec2 - kamangir/bolt#722

--- .abcli/install/kaggle.sh
+++ .abcli/install/kaggle.sh
@@ -4,10 +4,6 @@ function abcli_install_kaggle() {
     # https://github.com/Kaggle/kaggle-api
     pip3 install kaggle
 
-    mkdir -p ~/.kaggle/
-    cp -v \
-        $abcli_path_bash/bootstrap/config/kaggle.json \
-        ~/.kaggle/
     chmod 600 ~/.kaggle/kaggle.json
 }
 
