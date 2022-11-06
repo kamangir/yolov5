e570358e3b30fe34a7a7747477b8cd2e35172af7
kamangir <arash@kamangir.net>
2022-11-05 18:33:05 -0700

    validating yolov5 train coco128 - kamangir/bolt#722

--- .abcli/constants.sh
+++ .abcli/constants.sh
@@ -2,3 +2,4 @@
 
 export YOLOV5_MODEL_SIZES="yolov5n|yolov5s|yolov5m|yolov5l|yolov5x"
 export WANDB_MODE=disabled
+export KMP_DUPLICATE_LIB_OK=TRUE
--- yolov5/__init__.py
+++ yolov5/__init__.py
@@ -1,3 +1,3 @@
 NAME = "yolov5"
 
-VERSION = "3.50.1"
+VERSION = "3.51.1"
